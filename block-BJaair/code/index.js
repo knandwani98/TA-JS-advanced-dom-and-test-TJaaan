f0e1d2377bede56bc0048ddcca64f28fba5c3085
Kushal Nandwani <82793018+knandwani98@users.noreply.github.com>
2022-09-02 13:10:15 +0530

    f

--- block-BJaair/code/index.js
+++ block-BJaair/code/index.js
@@ -1,27 +1,76 @@
 let root = document.querySelector(".root");
 let form = document.querySelector("form");
-let inputTitle = document.querySelector(".input-title");
-let inputCategory = document.querySelector(".input-category");
 
-function addNotice() {
-  let div = document.createElement("div");
-  div.classList.add("card");
-  let a = document.createElement("a");
-  a.href = "#";
-  a.innerText = inputCategory.value;
-  let h2 = document.createElement("h2");
-  h2.innerText = inputTitle.value;
-  div.append(a, h2);
-  root.append(div);
+let cards = JSON.parse(localStorage.getItem("allNotice")) || [];
+
+function handleDelete(event) {
+  cards.splice(event.target.id, 1);
+  createUI();
+}
+
+function editText(event, id, label) {
+  let elm = event.target;
+  let txt = event.target.innerText;
+  let parent = event.target.parentElement;
+  let input = document.createElement("input");
+  parent.replaceChild(input, elm);
+  input.focus();
+  input.value = txt;
+  input.addEventListener("keyup", (event) => {
+    if (event.keyCode === 13) {
+      let updatedValue = event.target.value;
+      cards[id][label] = updatedValue;
+      createUI();
+      localStorage.setItem("allNotice", JSON.stringify(cards));
+    }
+  });
+}
+
+function createUI(data = cards, rooot = root) {
+  root.innerHTML = "";
+  let fragment = new DocumentFragment();
+  data.forEach((card, i) => {
+    let li = document.createElement("li");
+    li.classList.add("card");
+    let a = document.createElement("a");
+    a.href = "#";
+    a.innerText = card.category;
+    let h2 = document.createElement("h2");
+    h2.innerText = card.title;
+    let div = document.createElement("div");
+    let span = document.createElement("span");
+    span.id = i;
+    span.innerText = "x";
+
+    a.addEventListener("dblclick", (event) => {
+      editText(event, i, "category");
+    });
+
+    h2.addEventListener("dblclick", (event) => {
+      editText(event, i, "title");
+    });
+
+    span.addEventListener("click", (event) => {
+      handleDelete(event);
+      localStorage.setItem("allNotice", JSON.stringify(cards));
+    });
+
+    div.append(span);
+    li.append(a, h2, div);
+    fragment.appendChild(li);
+  });
+  root.append(fragment);
 }
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  addNotice();
-  inputTitle.value = "";
-  inputCategory.value = "";
+  let title = event.target.elements.title.value;
+  let category = event.target.elements.category.value;
+  cards.push({ title, category });
+  localStorage.setItem("allNotice", JSON.stringify(cards));
+  createUI();
+  event.target.elements.title.value = "";
+  event.target.elements.category.value = "";
 });
 
-let area = "";
-
-let editArea = function () {};
+createUI();
